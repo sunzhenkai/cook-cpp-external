5f4ac4b81aff3789fe65166c6946062bf88bdb23
wii <zhenkai.sun@qq.com>
2022-12-08 00:28:03 +0800

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "iostream"
 #include "fmt/format.h"
+#include "boost/filesystem.hpp"
 
 int main() {
     std::cout << fmt::format("{}", "ok fmt") << std::endl;
+    boost::filesystem::file_status fs;
+    fs.type();
     return 0;
 }
